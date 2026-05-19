118d3cd93081a0e085e9d94d4eacc778821ece81
qaswar114 <qaswar114@gmail.com>
2026-05-19 15:58:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(50), nullable=False)
+    assigned_grade = db.Column(db.String(50), default="")
 
 class Setting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -114,6 +115,11 @@ def get_settings():
 
 def init_database():
     db.create_all()
+    try:
+    db.session.execute(db.text("ALTER TABLE \"user\" ADD COLUMN assigned_grade VARCHAR(50) DEFAULT ''"))
+    db.session.commit()
+except Exception:
+    db.session.rollback()
     if not Setting.query.first():
         db.session.add(Setting(school_name=SCHOOL_NAME, address="Umar Faruq Integrated Academy"))
     default_users = [
