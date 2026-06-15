9f4c3d80d2170235999048990ea38bc5e971fd2d
qaswar114 <qaswar114@gmail.com>
2026-06-15 01:49:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,11 @@ database_url = os.environ.get(
 database_url = os.environ.get("DATABASE_URL", "sqlite:///school.db")
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
+
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY", "umar-faruq-secret-key-2026")
+
+
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
