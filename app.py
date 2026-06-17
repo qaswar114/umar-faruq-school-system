cca07875efb1ff31458c974d20d16dfae210e9d5
qaswar114 <qaswar114@gmail.com>
2026-06-17 08:01:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4766,14 +4766,14 @@ def payroll():
             "July", "August", "September", "October", "November", "December"
         ]
 
-       return render_template(
-           "payroll.html",
-            settings=get_settings(),
-            staff_options=staff_options,
-            payroll_rows=payroll_rows,
-            months=months,
-            total_net_salary=total_net_salary,
-            money=money
+        return render_template(
+            "payroll.html",
+             settings=get_settings(),
+             staff_options=staff_options,
+             payroll_rows=payroll_rows,
+             months=months,
+             total_net_salary=total_net_salary,
+             money=money
        )
     except Exception as e:
         import traceback
